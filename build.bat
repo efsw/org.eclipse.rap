1caf559c127522e1e2eb0491a0c3b5a8e49431dd
ezanaga1 <ezanaga@efsw.it>
2024-02-01 09:07:30 +0100

    file build relativo alla root

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-set java_home=C:\Shadow-tools\win64\java
-C:\Shadow-tools\maven\bin\mvn -Dmaven.test.skip=true clean verify
+set java_home=\Shadow-tools\win64\java
+\Shadow-tools\maven\bin\mvn -Dmaven.test.skip=true clean verify
 pause
